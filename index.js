149fa67549c8b3d4bcb80c2e3e42a81385570f92
Agne Klimaite <agnaite@me.com>
2017-11-08 16:43:15 -0800

    now displaying json on submit

--- index.js
+++ index.js
@@ -2,13 +2,17 @@
 
   var form = document.getElementsByTagName('form')[0];
   var addButton = document.getElementsByClassName('add')[0];
+  var submitButton = document.getElementsByTagName('button')[1];
   var relField = document.getElementsByName('rel')[0];
   var ageField = document.getElementsByName('age')[0];
+  var smokerField = document.getElementsByName('smoker')[0];
+  var debugEl = document.getElementsByClassName('debug')[0];
   var relValid = false;
   var ageValid = false;
-  var houseHold = [];
+  var household = [];
 
   updateAddButton();
+  updateSubmitButton();
 
   form.addEventListener('submit', submitForm);
   addButton.addEventListener('click', addPerson);
@@ -26,6 +30,13 @@
     evt.preventDefault();
 
     console.log('can submit')
+
+    household.push({ age: ageField.value,
+                     rel: relField.value,
+                     smoker: smokerField.checked
+                  });
+    updateSubmitButton();
+    console.log(household);
     // display person in html
       // with button to delete
   }
@@ -35,6 +46,10 @@
   function submitForm(evt) {
     evt.preventDefault();
     console.log('submitting form');
+    var jsonSubmission = JSON.stringify(household);
+
+    debugEl.innerHTML = jsonSubmission;
+    debugEl.style.display = 'block';
 
   }
 
@@ -49,6 +64,14 @@
       disableButton(addButton, false);
     } else {
       disableButton(addButton, true);
+    }
+  }
+
+  function updateSubmitButton() {
+    if (household.length > 0) {
+      disableButton(submitButton, false);
+    } else {
+      disableButton(submitButton, true);
     }
   }
 
