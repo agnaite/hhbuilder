e6a67983cf0a1ef74648d8e1bbfc854124b60960
Agne Klimaite <agnaite@me.com>
2017-11-08 16:17:36 -0800

    added validations

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 (function() {
+
   var form = document.getElementsByTagName('form')[0];
   var addButton = document.getElementsByClassName('add')[0];
+  var relField = document.getElementsByName('rel')[0];
+  var ageField = document.getElementsByName('age')[0];
+  var relValid = false;
+  var ageValid = false;
+  var houseHold = [];
+
+  notValid(addButton, true);
 
   form.addEventListener('submit', submitForm);
   addButton.addEventListener('click', addPerson);
 
+  relField.addEventListener('change', function() {
+    validateRel(relField.value);
+    if (ageValid && relValid) {
+      notValid(addButton, false);
+    }
+  });
+  ageField.addEventListener('change', function() {
+    validateAge(ageField.value)
+    if (ageValid && relValid) {
+      notValid(addButton, false);
+    }
+  });
+
   function addPerson(evt) {
     evt.preventDefault();
-    console.log('adding person');
-    // check age < 0
-    // check relationship not null
+
+    console.log('can submit')
     // display person in html
       // with button to delete
   }
@@ -19,6 +39,41 @@
   function submitForm(evt) {
     evt.preventDefault();
     console.log('submitting form');
+
+  }
+
+  // helpers
+
+  function notValid(el, status) {
+    el.disabled = status;
+  }
+
+  function validateAge(age) {
+    if (isNotEmpty(age) && parseInt(age) > 0) {
+      ageValid = true;
+    } else {
+      console.log('age required and has to be > 0.')
+      ageValid = false;
+      notValid(addButton, true);
+    }
+  }
+
+  function validateRel(rel) {
+    if (isNotEmpty(rel)) {
+      relValid = true;
+    } else {
+      console.log('relationship required.')
+      relValid = false;
+      notValid(addButton, true);
+    }
+  }
+
+  function isNotEmpty(value) {
+    if (value !== "") {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 })();
